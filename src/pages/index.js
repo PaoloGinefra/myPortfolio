5c0070a9c6e65781f20ac88c9bcbf19098e7650d
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-05 12:10:39 +0100

    feat: basic site done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYouTube, AiFillYoutube} from 'react-icons/ai'
+import Image from "next/image"
+import deved from "../../public/dev-ed-wave.png"
+import design from '../../public/design.png'
+import code from '../../public/code.png'
+import consulting from '../../public/consulting.png'
+import web1 from '../../public/web1.png'
+import web2 from '../../public/web2.png'
+import web3 from '../../public/web3.png'
+import web4 from '../../public/web4.png'
+import web5 from '../../public/web5.png'
+import web6 from '../../public/web6.png'
 
 export default function Home() {
   return (
@@ -11,19 +23,122 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-black px-10'>
+      <main className='bg-black px-10 md:px-20 lg:px-40'>
         <section className=' min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl'>
               developed by
             </h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
               <li><a
               className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#"
               >Resume</a></li>
             </ul>
           </nav>
+
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Paolo Ginefra</h2>
+            <h3 className='text-2xl py-2 md:text-3xl'>Developer</h3>
+
+            <p className='text-md py-5 leading-8 text-gray-400 md:text-xl max-w-lg mx-auto'>
+              Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur.
+            </p>
+          </div>
+
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-200'>
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+            <Image src = {deved} layout='fill' objectFit='cover' alt='ME'/>
+          </div>
+
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>
+              Services I offer
+            </h3>
+
+            <p className='text-md py-2 leading-8 text-gray-400'>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+
+          <div className='lg:flex gap-10'>
+            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
+              <Image src = {design} alt = 'design' width={100} height={100} className='relative mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet, consectetur adipisci elit
+              </p>
+
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-400 py-1'>Photoshop</p>
+              <p className='text-gray-400 py-1'>Illustrator</p>
+              <p className='text-gray-400 py-1'>After Effect</p>
+            </div>
+
+            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
+              <Image src = {code} alt = 'design' width={100} height={100} className='relative mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Code</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet, consectetur adipisci elit
+              </p>
+
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-400 py-1'>Photoshop</p>
+              <p className='text-gray-400 py-1'>Illustrator</p>
+              <p className='text-gray-400 py-1'>After Effect</p>
+            </div>
+
+            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
+              <Image src = {consulting} alt = 'design' width={100} height={100} className='relative mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet, consectetur adipisci elit
+              </p>
+
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-400 py-1'>Photoshop</p>
+              <p className='text-gray-400 py-1'>Illustrator</p>
+              <p className='text-gray-400 py-1'>After Effect</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <p className='text-md py-2 leading-8 text-gray-400'>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+
+          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web1} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web2} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web3} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web4} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web5} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src = {web6} alt = 'web1' className='rounded-lg object-cover' width={'100%'} layout='responsive'/>
+            </div>
+          </div>
+
         </section>
       </main>
     </>
