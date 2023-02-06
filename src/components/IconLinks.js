47fdc523ee607575e8723070c0dbea3ee3531684
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-06 20:43:35 +0100

    chore: added github link to the icons section

--- src/components/IconLinks.js
+++ src/components/IconLinks.js
@@ -1,8 +1,9 @@
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram, AiFillYoutube} from 'react-icons/ai'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram, AiFillYoutube, AiFillGithub} from 'react-icons/ai'
 
 function IconLinks(){
     return (
         <div className='text-5xl flex justify-center gap-16 py-2 text-gray-200'>
+            <a href="https://github.com/PaoloGinefra" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
             <a href="https://twitter.com/PaoloGinefra" target="_blank" rel="noopener noreferrer"><AiFillTwitterCircle /></a>
             <a href="https://www.instagram.com/paolo_ginefra/" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></a>
             <a href="https://www.youtube.com/channel/UC9tieCBEaNN8EgO5rXio2Kw" target="_blank" rel="noopener noreferrer"><AiFillYoutube /></a>
