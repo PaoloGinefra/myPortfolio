351599243b1b496ac55f1ea20fd1f11ae9324a8e
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-06 15:30:25 +0100

    feat: added the tool card

--- src/components/Tools/ToolsSection.js
+++ src/components/Tools/ToolsSection.js
@@ -1,61 +1,22 @@
-import Image from "next/image"
-
-import design from '../../../public/design.png'
-import code from '../../../public/code.png'
-import consulting from '../../../public/consulting.png'
+/* eslint-disable react/jsx-key */
+import ToolCard from "./ToolCard"
+import Tools from '../../../public/data/Tools.json'
 
 function ToolsSection({imageSrc}){
     return (
         <section>
           <div>
-            <h3 className='text-3xl py-1'>
-              Services I offer
+            <h3 className='text-3xl py-1 text-center '>
+              Tools I use
             </h3>
 
-            <p className='text-md py-2 leading-8 text-gray-400'>
-            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
           </div>
 
-          <div className='lg:flex gap-10'>
-            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
-              <Image src = {design} alt = 'design' width={100} height={100} className='relative mx-auto'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-              <p className='py-2'>
-                Lorem ipsum dolor sit amet, consectetur adipisci elit
-              </p>
+          <div className='flex flex-wrap gap-16 justify-center flex-grow-0'>
+            {Tools.map((tool) => {
+                return <ToolCard title={tool.Name} imgSrc={tool.ImgSrc} status={'Advanced'}/>
+              })}
 
-              <h4 className='py-4 text-[color:var(--primary)]'>Design tools I use</h4>
-              <p className='text-gray-400 py-1'>Photoshop</p>
-              <p className='text-gray-400 py-1'>Illustrator</p>
-              <p className='text-gray-400 py-1'>After Effect</p>
-            </div>
-
-            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
-              <Image src = {code} alt = 'design' width={100} height={100} className='relative mx-auto'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Code</h3>
-              <p className='py-2'>
-                Lorem ipsum dolor sit amet, consectetur adipisci elit
-              </p>
-
-              <h4 className='py-4 text-[color:var(--primary)]'>Design tools I use</h4>
-              <p className='text-gray-400 py-1'>Photoshop</p>
-              <p className='text-gray-400 py-1'>Illustrator</p>
-              <p className='text-gray-400 py-1'>After Effect</p>
-            </div>
-
-            <div className='text-center shadow-lg shadow-gray-600 p-10 rounded-xl my-10'>
-              <Image src = {consulting} alt = 'design' width={100} height={100} className='relative mx-auto'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
-              <p className='py-2'>
-                Lorem ipsum dolor sit amet, consectetur adipisci elit
-              </p>
-
-              <h4 className='py-4 text-[color:var(--primary)]'>Design tools I use</h4>
-              <p className='text-gray-400 py-1'>Photoshop</p>
-              <p className='text-gray-400 py-1'>Illustrator</p>
-              <p className='text-gray-400 py-1'>After Effect</p>
-            </div>
           </div>
         </section>
     )
