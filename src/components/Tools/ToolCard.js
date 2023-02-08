7f3bc9e26e35888cee3e286efd322d9400fd5777
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-08 13:00:19 +0100

    chore: updated image alts

--- src/components/Tools/ToolCard.js
+++ src/components/Tools/ToolCard.js
@@ -5,7 +5,7 @@ function ToolCard({title, imgSrc, status}){
         <div className="flex flex-col max-w-[20vmax] mt-10 relative">
             <div className="flex-none group relative w-full aspect-square">
                 <div className=" mx-auto aspect-square outline outline-8 outline-offset-4 outline-[color:var(--primary)] rounded-full overflow-hidden [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] transition-all duration-500">
-                    <Image className="w-full" src={imgSrc} alt="Sunset in the mountains" width={100} height={100}/>
+                    <Image className="w-full" src={imgSrc} alt={title} width={100} height={100}/>
                 </div>
 
                 <div className="absolute flex aspect-square top-0 bg-white bg-opacity-80 h-full w-full justify-center align-middle [transform:rotateY(180deg)] rounded-full [backface-visibility:hidden] group-hover:[transform:rotateY(360deg)] transition-all duration-500">
--- src/components/Tools/ToolTag.js
+++ src/components/Tools/ToolTag.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function ToolTag({tool}){
     return(
         <span className="inline-block aspect-square bg-gray-200 outline outline-white rounded-full mr-2 mb-2 overflow-hidden">
-            <Image className="w-full" src = {tool.ImgSrc} alt={tool.name} width={20} height={0}/>
+            <Image className="w-full" src = {tool.ImgSrc} alt={tool.Name} width={20} height={0}/>
         </span>
     );
 }
