ff2023d45e0069926be8e094ba508677b0f99538
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-09 21:57:45 +0100

    chore: adjusted tool card icon and functionality

--- src/components/Tools/ToolCard.jsx
+++ src/components/Tools/ToolCard.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import { useState } from "react";
 import { motion } from "framer-motion"
+import {BiShow, BiHide} from 'react-icons/bi'
 
 const variants = {
     selected: { opacity: 1,},
-    notSelected: { opacity: 0.3,},
+    notSelected: { opacity: 0.2,},
 }
 
 function ToolCard({tool, toggleSelectTool}){
@@ -15,23 +16,23 @@ function ToolCard({tool, toggleSelectTool}){
         animate={Selected ? "selected" : "notSelected"}
         variants={variants}
         >
-            <div className="flex-none group relative w-full aspect-square">
+            <button
+            className="flex-none group relative w-full aspect-square"
+            onClick={() => {
+                toggleSelectTool(tool.id);
+                setSelected(!Selected);
+            }}
+            >
                 <div className=" mx-auto aspect-square outline outline-8 outline-offset-4 outline-[color:var(--primary)] rounded-full overflow-hidden [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] transition-all duration-500">
                     <Image className="w-full" src={tool.ImgSrc} alt={tool.Name} width={100} height={100}/>
                 </div>
 
                 <div className="absolute flex aspect-square top-0 bg-white bg-opacity-80 h-full w-full justify-center align-middle [transform:rotateY(180deg)] rounded-full [backface-visibility:hidden] group-hover:[transform:rotateY(360deg)] transition-all duration-500">
-                    <button
-                        className="m-auto bg-[color:var(--primary-darkened)] p-2 rounded-full text-sm font-semibold text-white text-center"
-                        onClick={() => {
-                            toggleSelectTool(tool.id);
-                            setSelected(!Selected);
-                        }}
-                    >
-                        {Selected ? 'Hide' : 'Select'}
-                    </button>
+                    <p className="m-auto bg-[color:var(--primary-darkened)] p-2 rounded-full text-3xl font-semibold text-white text-center">
+                        {Selected ? <BiShow/> : <BiHide/>}
+                    </p>
                 </div>
-            </div>
+            </button>
 
 
 
