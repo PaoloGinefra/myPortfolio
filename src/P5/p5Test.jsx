b857f907d073353e68ffb1de6d63311ac4cc08aa
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-02-18 22:17:33 +0100

    feat: added nPenulum IK

--- src/P5/p5Test.jsx
+++ src/P5/p5Test.jsx
@@ -21,6 +21,9 @@ const nTails = 500;
 
 const velNormScale = 0.2;
 const normMin = 0.03;
+const maxNorm = 8;
+
+const IK_target_lerp = 0.1;
 
 let masses = [Infinity];
 let angles = [];
@@ -32,6 +35,10 @@ let tails = [];
 
 function getScreenPos(pos) {
   return [pos.x + width / 2, pos.y + width / 2];
+}
+
+function getWorldPos(screenPos) {
+  return [screenPos.x - width / 2, screenPos.y - width / 2];
 }
 
 function P5Test({ props }) {
@@ -82,7 +89,7 @@ function P5Test({ props }) {
       p5.line(x1, y1, x2, y2);
     }
 
-    applyConstraint(p5) {
+    applyConstraint(p5, blockB = false) {
       const totalW = 1 / this.mA.mass + 1 / this.mB.mass;
       const dist = p5.constructor.Vector.sub(
         this.mA.position,
@@ -94,11 +101,69 @@ function P5Test({ props }) {
       ).normalize();
       const correction = this.lenght - dist;
 
-      const dp_mA = dir.copy().mult(correction / this.mA.mass / totalW);
+      const dp_mA = dir
+        .copy()
+        .mult(correction * (blockB ? 1 : 1 / this.mA.mass / totalW));
       this.mA.position.add(dp_mA);
 
-      const dp_mB = dir.copy().mult(-correction / this.mB.mass / totalW);
+      const dp_mB = dir
+        .copy()
+        .mult(-correction * (blockB ? 0 : 1 / this.mB.mass / totalW));
       this.mB.position.add(dp_mB);
+    }
+  }
+
+  function simulate(p5) {
+    for (let sample = 0; sample < subSemples; sample++) {
+      for (let i = 1; i < Masses.length; i++) {
+        Masses[i].velocity.add(p5.createVector(0, (g * dt) / subSemples));
+        Masses[i].prevPosition = Masses[i].position.copy();
+        Masses[i].position.add(Masses[i].velocity.mult(dt / subSemples));
+      }
+
+      for (let i = 0; i < Links.length; i++) {
+        Links[i].applyConstraint(p5);
+      }
+
+      for (let i = 1; i < Masses.length; i++) {
+        Masses[i].velocity = p5.constructor.Vector.sub(
+          Masses[i].position,
+          Masses[i].prevPosition
+        ).mult(subSemples / dt);
+      }
+    }
+  }
+
+  function isMouseInFrame(p5) {
+    return (
+      p5.mouseX >= 0 && p5.mouseX < width && p5.mouseY >= 0 && p5.mouseY < width
+    );
+  }
+
+  function IK(p5) {
+    console.log(p5.createVector(p5.mouseX, p5.mouseY));
+    const [tx, ty] = getWorldPos(p5.createVector(p5.mouseX, p5.mouseY));
+    const target = p5.createVector(tx, ty);
+
+    for (let i = 1; i < Masses.length; i++) {
+      Masses[i].prevPosition = Masses[i].position.copy();
+    }
+
+    Masses[nMasses].position.lerp(target, IK_target_lerp);
+    for (let i = nMasses - 1; i >= 0; i--) {
+      Links[i].applyConstraint(p5, true);
+    }
+
+    const offset = Masses[0].position.copy();
+    for (let i = 0; i <= nMasses; i++) {
+      Masses[i].position.sub(offset);
+    }
+
+    for (let i = 1; i < Masses.length; i++) {
+      Masses[i].velocity = p5.constructor.Vector.sub(
+        Masses[i].position,
+        Masses[i].prevPosition
+      ).mult(subSemples / dt);
     }
   }
 
@@ -134,68 +199,53 @@ function P5Test({ props }) {
     for (let i = 0; i < lengths.length; i++) {
       Links.push(new Link(Masses[i], Masses[i + 1], lengths[i]));
     }
-
-    console.log(Masses);
-    console.log(Links);
   };
 
   const draw = (p5) => {
     p5.clear();
 
-    p5.stroke(255);
+    if (p5.mouseIsPressed && p5.mouseButton === "left") {
+      IK(p5);
+      tails = [];
+    } else {
+      simulate(p5);
 
-    for (let sample = 0; sample < subSemples; sample++) {
-      for (let i = 1; i < Masses.length; i++) {
-        Masses[i].velocity.add(p5.createVector(0, (g * dt) / subSemples));
-        Masses[i].prevPosition = Masses[i].position.copy();
-        Masses[i].position.add(Masses[i].velocity.mult(dt / subSemples));
+      tails.push({
+        p1: p5.constructor.Vector.add(
+          Masses[Masses.length - 1].position,
+          Masses[Masses.length - 1].velocity
+            .copy()
+            .mult((velNormScale + normMin) / 2)
+            .rotate(p5.HALF_PI)
+            .limit(maxNorm)
+        ),
+        p2: p5.constructor.Vector.add(
+          Masses[Masses.length - 1].position,
+          Masses[Masses.length - 1].velocity
+            .copy()
+            .mult((velNormScale + normMin) / 2)
+            .rotate(-p5.HALF_PI)
+            .limit(maxNorm)
+        ),
+      });
+
+      if (tails.length > nTails) tails.shift();
+
+      p5.noStroke();
+      p5.fill(255);
+      p5.beginShape();
+      for (let i = 0; i < tails.length; i++) {
+        const [x1, y1] = getScreenPos(tails[i].p1);
+        p5.curveVertex(x1, y1);
       }
 
-      for (let i = 0; i < Links.length; i++) {
-        Links[i].applyConstraint(p5);
+      for (let i = tails.length - 1; i >= 0; i--) {
+        const [x2, y2] = getScreenPos(tails[i].p2);
+
+        p5.curveVertex(x2, y2);
       }
-
-      for (let i = 1; i < Masses.length; i++) {
-        Masses[i].velocity = p5.constructor.Vector.sub(
-          Masses[i].position,
-          Masses[i].prevPosition
-        ).mult(subSemples / dt);
-      }
+      p5.endShape();
     }
-
-    tails.push({
-      p1: p5.constructor.Vector.add(
-        Masses[Masses.length - 1].position,
-        Masses[Masses.length - 1].velocity
-          .copy()
-          .mult((velNormScale + normMin) / 2)
-          .rotate(p5.HALF_PI)
-      ),
-      p2: p5.constructor.Vector.add(
-        Masses[Masses.length - 1].position,
-        Masses[Masses.length - 1].velocity
-          .copy()
-          .mult((velNormScale + normMin) / 2)
-          .rotate(-p5.HALF_PI)
-      ),
-    });
-
-    if (tails.length > nTails) tails.shift();
-
-    p5.noStroke();
-    p5.fill(255);
-    p5.beginShape();
-    for (let i = 0; i < tails.length; i++) {
-      const [x1, y1] = getScreenPos(tails[i].p1);
-      p5.vertex(x1, y1);
-    }
-
-    for (let i = tails.length - 1; i >= 0; i--) {
-      const [x2, y2] = getScreenPos(tails[i].p2);
-
-      p5.vertex(x2, y2);
-    }
-    p5.endShape();
 
     Links.forEach((link) => link.draw(p5));
     Masses.forEach((mass) => mass.draw(p5));
