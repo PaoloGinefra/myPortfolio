13b41f37a9db281afef3b578955eff3ed6102f26
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-04-14 15:24:06 +0200

    chore: partially filled the student section and minor adjustments

--- public/data/Titles.js
+++ public/data/Titles.js
@@ -31,11 +31,11 @@ const Titles = {
 
   Student: {
     hue: 50,
-    Description: `Hi there I am a student`,
-    ProjectsDescription: `Lorem ipsum dolor sit amet, consectetur adipisci elit,
-    sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam,
-    nisi ut aliquid ex ea commodi consequatur.`,
+    Description: `Hi there 👋,
+    I have been a full time student since 2008.
+    I know, impressive right? All jokes aside, I've always teken my education very seriously as a source of knowledge and a way to satify my curiosity.
+    Also it lead to a lot of great friends and life changing experiences.`,
+    ProjectsDescription: `In this sections you can find some of the projects I have worked on during my studies as well as a summary of my academic achievements.`,
   },
 
   Maker: {
